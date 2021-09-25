d99311d13f62df1f6a3584b18389339e24f08062
zubairhmd <zubairhmd84@gmail.com>
2021-09-26 01:40:15 +0600

    install bootstrap & fontawesome

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+const element = <FontAwesomeIcon icon={faCoffee} />
 
 function App() {
   return (
     <div>
-      <h1>Create React File</h1>
+      <h1 className='title'>{element} Create React File</h1>
     </div>
   );
 }
